a10b79debe45e10b45b4c7609eb4b3cd97ad7ee9
Chonapatc <kilito2546@gmail.com>
2023-12-18 22:42:24 +0700

    kjhjk

--- collec.c
+++ collec.c
@@ -8,6 +8,37 @@ int main()
 
     fgets(a,100,stdin);
 
-    int a,b,
+    double goal=atoi(a),sum=0,temp;
 
+    int day=0;
+
+    while(sum<goal)
+    {
+        if(day>0)
+        {
+            printf("\n");
+        }
+        printf("Enter money collected today: ");
+        fgets(a,100,stdin);
+        double val=atof(a);
+        if(val<=0)
+        {
+            continue;
+        }
+        sum+=val;
+        day++;
+        if(sum>=goal)
+        {
+            break;
+        }
+        printf("Total money collected up to day %d is %.2f. You need %.2f more.",day,sum,goal-sum);
+    }
+    char s='\0';
+    if(day!=1)
+    {
+        s='s';
+    }
+
+
+    printf("Congratulations! You take %d day%c to reach your goal.",day,s);
 }
